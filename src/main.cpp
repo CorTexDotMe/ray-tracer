94d3c5a83b87fbf9efce65639c5051e2e888f20c
Danylo Nechyporchuk <danylo.nechyporchuk@gmail.com>
2023-08-15 19:51:39 +0300

    fix debug version crush

--- src/main.cpp
+++ src/main.cpp
@@ -133,7 +133,7 @@ int main(int, char**)
         ImGui::Text("Size = %d x %d", rayTracer->getImageWidth(), rayTracer->getImageHeight());
         ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
         ImGui::End();
-        ImGui::PopStyleVar(2);
+        ImGui::PopStyleVar(1);
 
 
         
